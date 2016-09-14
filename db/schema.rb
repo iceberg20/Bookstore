376c54273c09479791ed73b1daa629602aa7911e
iceberg20 <italoberg@yahoo.com.br>
2016-09-14 10:13:24 -0300

    seed was created to populate initial values to the database

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20160913160005) do
 
-  create_table "sales", force: true do |t|
+  create_table "sales", force: :cascade do |t|
     t.string   "book"
     t.decimal  "price"
     t.datetime "created_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Sale.create(book: 'Web Development With Rails', price: 50, 
+						image_url: 'http://migre.me/uYyxf')
+
+Sale.create(book: 'Abra sua cabeça - Rails', price: 10, 
+						image_url: 'http://migre.me/uYzJj')
+
+Sale.create(book: 'O monge e o executivo', price: 26, 
+						image_url: 'http://migre.me/uYOlr')
+
+Sale.create(book: 'Almoço', price: -15, 
+						image_url: 'http://migre.me/uYOsV')
+
